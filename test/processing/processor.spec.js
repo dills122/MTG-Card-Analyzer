04765c767338fc0dec0bc67ff2674ae9a6980cf4
Dylan Steele <dylansteele57@gmail.com>
2019-05-25 13:34:17 -0400

    added another test case

--- test/processing/processor.spec.js
+++ test/processing/processor.spec.js
@@ -1,27 +1,38 @@
 const chai = require('chai');
-const expect = chai.expect;
-const sinon = require('sinon');
 const assert = require('assert');
 const processor = require('../../src/processor/processor');
 const path = require('path');
 const appRoot = require('app-root-path');
-const filePath = path.join(appRoot.toString(),'src\\test-images\\test-extractions\\93e73461-807b-4590-9171-c3759a2576e9.jpg');
 
 describe("Processor::", () => {
-    let sandbox = {};
-    let stubs = {};
     beforeEach(() => {
-        sandbox = sinon.createSandbox();
+
     });
     afterEach(() => {
-        sinon.restore();
+
     });
     describe("ProcessImage::", () => {
-        it("Should throw to small error", (done) => {
+        it("Should process single image", (done) => {
+            const filePath = path.join(appRoot.toString(),'src\\test-images\\PlatinumAngel.jpg');
             let imgProcessor = processor.create({
                 filePath: filePath
             });
             imgProcessor.execute((err) => {
+                assert.equal(imgProcessor.filePath, filePath);
+                chai.assert.isAbove(imgProcessor.nameMatches[0][0], .65);
+                chai.assert.deepEqual(imgProcessor.nameMatches[0][1], 'Platinum Angel');
+                chai.assert.isAbove(imgProcessor.typeMatches[0][0], .75);
+                chai.assert.deepEqual(imgProcessor.typeMatches[0][1], 'Creature');
+                done();
+            });
+        }).timeout(5000);
+        it("Should throw to small error", (done) => {
+            const filePath = path.join(appRoot.toString(),'src\\test-images\\test-extractions\\93e73461-807b-4590-9171-c3759a2576e9.jpg');
+            let imgProcessor = processor.create({
+                filePath: filePath
+            });
+            imgProcessor.execute((err) => {
+                //Error throwing not bubbling up
                 assert.equal(imgProcessor.filePath, filePath);
                 chai.assert.isNull(imgProcessor.nameMatches);
                 assert.deepEqual(imgProcessor.typeMatches, []);
@@ -29,4 +40,4 @@ describe("Processor::", () => {
             });
         });
     });
-});
+}).timeout(10000);
