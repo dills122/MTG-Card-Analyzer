b8edc9f81ce66d2d24ea1e25d67045b05c8def03
Paul Casey <jpcasey07@gmail.com>
2019-06-02 13:15:05 -0400

    Created secure.config.js template.

---
+++ secure.config.template.js
@@ -0,0 +1,11 @@
+// Used for the MySQL database connection.
+// Rename to 'secure.config.js' and enter your database credentials.
+
+module.exports = {
+    rds: {
+        host: '...',
+        database: '...',
+        user: '...',
+        password: '...'
+    }
+}
